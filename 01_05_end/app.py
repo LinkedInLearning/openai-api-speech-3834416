6a645c6a4d74e82c85268db1b5cf07a87839e7f1
Ronnie Sheer <ronysheer@gmail.com>
2023-12-19 14:07:18 +0200

    add  01_05_end

--- 01_05_end/app.py
+++ 01_05_end/app.py
@@ -5,3 +5,10 @@ from flask import Flask, render_template, send_file, request
 
 client = OpenAI()
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(debug=True)

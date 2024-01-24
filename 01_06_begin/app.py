8553accb3576b541762c91aeff82705d77641675
Ronnie Sheer <ronysheer@gmail.com>
2024-01-24 13:42:00 +0200

    update: app.py to begin state

--- 01_06_begin/app.py
+++ 01_06_begin/app.py
@@ -12,16 +12,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/tts", methods=["POST"])
-def tts():
-    data = request.get_json()
-    text = data.get("text", "")
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text,
-    )
-    return send_file(BytesIO(response.read()), mimetype="audio/mp3")
+
 
 
 app.run(debug=True)
